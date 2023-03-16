0b900aea87b99395a5b5afa2fb36746aab0d78cf
marsade <adenijifara@gmail.com>
2023-03-16 10:23:15 -0700

    Update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Function prints a string using printf
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
