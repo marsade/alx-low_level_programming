3c665e9eaf5646751a1e533d9248050c93cc5967
marsade <adenijifara@gmail.com>
2023-03-16 10:04:17 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
 
