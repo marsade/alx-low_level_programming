75e39ce428e4ccf69d83bbb4d6f3a2965e0354ae
marsade <adenijifara@gmail.com>
2023-03-16 10:10:05 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /* 
  * main - A function that prints with the put function
- * */
+ * Return - SUCCESS
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
