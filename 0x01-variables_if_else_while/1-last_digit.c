f4ab8bf89dbf3501ae07004d67fca2f1de26656a
marsade <adenijifara@gmail.com>
2023-03-17 15:11:53 -0700

    Update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This program will assign a random number to the variable n
+ * each time it is executed
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int lastDigit = n % 10;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	} else if (lastDigit == 0 )
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	} else if (lastDigit < 6 && !0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+	return (0);
+}
