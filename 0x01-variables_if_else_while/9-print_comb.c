1f897e282a4bbd603d47c8927ab6c4de4cb40843
marsade <adenijifara@gmail.com>
2023-03-17 17:31:58 -0700

    Update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This function prints all possible cmbinations of single
+ * digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if(i != 9)	
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
