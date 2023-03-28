2431fef2c7d88a19cc2d9940a09a07ed0183eb0b
marsade <adenijifara@gmail.com>
2023-03-28 10:48:17 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - takes a pointer as parameter and updates
+* the value it points to 98
+* @n: pointer
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
