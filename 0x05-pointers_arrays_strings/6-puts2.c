827bba98230fa4603f76aa162d557219bea66d6c
marsade <adenijifara@gmail.com>
2023-03-28 13:35:41 -0700

    Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,15 @@
 */
 void puts2(char *str)
 {
-	int count = 1, i;
+	int count = 0, i, j;
 
 	while (str[count] != '\0')
 		count++;
-	for (i = 0; i <= count; i++)
+	j = count -1;
+	for (i = 0; i <= j; i++)
 	{
-		if (i % 2 != 0)
-			i++;
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
