eaf978f6606e8c8f62eac64b1f17bf295d9e6aaa
marsade <adenijifara@gmail.com>
2023-03-30 13:57:59 -0700

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,15 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i;
+	int j;
 
-	while (*dest != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
-		dest++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -24,5 +26,4 @@ char *_strcat(char *dest, char *src)
 
 	dest[i] = '\0';
 	return (dest);
-
 }
