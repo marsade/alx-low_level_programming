3e5a983b47366110159ca8a7027f8b8b13450844
marsade <adenijifara@gmail.com>
2023-03-30 16:13:36 -0700

    Update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strcmp - compares two strings
+* @s1: string one
+* @s2: string two
+* Return: 15 if first character in s1 is greater than s2
+* 0 if equal
+* -15 if character in s1 is less than s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i, res;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			res = 0;
+		else if (s1[i] > s2[i])
+			res = -15;
+		else if (s1[i] < s2[i])
+			res = 15;
+		i++;
+	}
+	return (res);
+}
