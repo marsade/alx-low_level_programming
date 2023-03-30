0a838c1faa71bcd7a18a44d6c4015fe35ab68ba4
marsade <adenijifara@gmail.com>
2023-03-30 16:21:57 -0700

    Update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, res;
+	int i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-			res = 0;
-		else if (s1[i] > s2[i])
-			res = -15;
-		else if (s1[i] < s2[i])
-			res = 15;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (res);
+	return (0);
 }
