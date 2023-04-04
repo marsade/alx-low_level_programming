f36286d46d96100d0560b214b513a3b521de33a2
marsade <adenijifara@gmail.com>
2023-04-04 07:11:28 -0700

    Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,7 @@
 * _memset - fills the first n bytes of the memory
 * @n: number of bytes to be filled
 * @b: byte to be filled with
+* @s: character array
 * Return: a pointer to the memory area s
 */
 char *_memset(char *s, char b, unsigned int n)
