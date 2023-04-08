0d4317ad374d4fb8b63eba94d204e1a805e697f7
marsade <adenijifara@gmail.com>
2023-04-08 12:49:03 -0700

    Update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+* num_check - check if the string is a digit
+* @strn: string to be checked
+* Return 0
+*/
+int num_check(char *strn)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(strn))
+	{
+		if (!isdigit(strn[count]))
+			return (0);
+		count++;
+	}
+	return (1);
+}
+/**
+* main - adds positive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0
+*/
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	int i, chg_str, sum = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (num_check(argv[i]))
+		{
+			chg_str = atoi(argv[i]);
+			sum += chg_str;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
