915bcdbb1b84f32b4403d062550f9cde03f8e5f6
marsade <adenijifara@gmail.com>
2023-04-08 06:59:56 -0700

    Update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/**
+* main - check code
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my
+			goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
