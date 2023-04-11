a3b1ab5f9ed7ed87afe1ff6b580309567c2269f7
marsade <adenijifara@gmail.com>
2023-04-11 06:23:34 -0700

    Update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(sizeof(char) * size);
 
-	if (size != 0)
+	if (size != 0 || ptr != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
