a60eea8fc9ba0bbd0e69cba5738fcad826445fd5
marsade <adenijifara@gmail.com>
2023-04-17 04:12:23 -0700

    Update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - creates a new struct type of dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
