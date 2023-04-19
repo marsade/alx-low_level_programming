61801634b70153603e3a3eb8aa0d1655101733cf
marsade <adenijifara@gmail.com>
2023-04-19 03:16:00 -0700

    Update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	for (i = 0; i <= size; i++)
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
