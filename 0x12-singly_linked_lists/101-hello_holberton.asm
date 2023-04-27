337fbe4ddcf52d9348f5ad857f19455e5505e4d9
marsade <adenijifara@gmail.com>
2023-04-27 14:08:48 -0700

    Task 6 complete

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	message db 'Hello, Holberton', 0
+	format db '%s', 10, 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	mov rdi, format
+	mov rsi, message
+	xor eax, eax
+	call printf
+
+	xor edi, edi
+	mov eax, 60
+	syscall
