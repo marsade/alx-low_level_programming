1b7c40e76adc54ccd49ef38d4010570b34c97c11
marsade <adenijifara@gmail.com>
2023-04-27 14:12:45 -0700

    Fix betty errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 void print(void) __attribute__((constructor));
 /**
-  * print: prints to terminal
+  * print - prints to sentence to the terminal before main
   */
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
