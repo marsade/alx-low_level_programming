80010393d4767b97d42161971b5e8eedafc1508f
marsade <adenijifara@gmail.com>
2023-04-28 05:33:52 -0700

    Task 0 complete

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * print_listint - prints all the elements of a linstint_t list
+  * @h: pointer to head of list
+  * Return: number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
