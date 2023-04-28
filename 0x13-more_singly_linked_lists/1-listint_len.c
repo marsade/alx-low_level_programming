ab0640f1781100a1d4b7ee53763c7052c8d850ad
marsade <adenijifara@gmail.com>
2023-04-28 07:48:38 -0700

    Task 1 complete

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,17 @@
+#include "lists.h"
+/**
+  * listint_len - returns the number of elements ina linked list
+  * @h: pointer to the head of the list
+  * Return: count
+  */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
 
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
