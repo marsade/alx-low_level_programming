35100b276dc5ad1482b92d620e935584c0642802
marsade <adenijifara@gmail.com>
2023-04-29 04:24:58 -0700

    Task 7 complete

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *temp;
 
+	if (head == NULL)
+		return (0);
+
 	while (head != NULL)
 	{
 		if (count == index)
 			temp = head;
+		if (temp == NULL)
+			return (NULL);
 		count++;
 		head = head->next;
 	}
