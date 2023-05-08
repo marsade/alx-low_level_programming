59512da5cc5b4d128cb9a1cd489894ec070b443c
marsade <adenijifara@gmail.com>
2023-05-08 05:23:23 -0700

    Initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+  * read_textfile - reads a text file and prints it to the POSIX standard
+  * output
+  * @filename: the file to be read
+  * @letters: the number of letters it should read and print
+  * Return: the actual number of letters it could read and print
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	while ((bytes_read = read(fd, buf, letters)) > 0)
+	{
+		bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			free(buf);
+			close(fd);
+			return (0);
+		}
+	}
+	free(buf);
+	close(fd);
+	return (bytes_written);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+#endif
