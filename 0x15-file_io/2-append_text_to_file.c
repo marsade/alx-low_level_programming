0b0e259c108fa4835eb60041018cac6f82029913
marsade <adenijifara@gmail.com>
2023-05-27 00:50:05 -0700

    Minor update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,20 +12,17 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
-		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
-	if (text_content)
+	if (text_content != NULL)
 	{
-		nwr = write(fd, text_content, len);
-		if (nwr == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len]; len++)
+			;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	nwr = write(fd, text_content, len);
+	if (nwr == -1 || fd == -1)
+	{
+		return (-1);
 	}
 	close(fd);
 	return (1);
