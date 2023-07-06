a3d7972161810244ce1c858d302d959f573fd82c
mars <adenijifara@gmail.com>
2023-07-06 10:07:18 -0700

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,5 +12,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 	mask = 1 << index;
-	return (*n |= mask);
+	*n |= mask;
+	return (1);
 }
