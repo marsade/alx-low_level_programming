f35e0b9237d9dfd0e35227be7778839549b7c02f
mars <adenijifara@gmail.com>
2023-08-03 13:32:50 -0700

    Fix betty error

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * get_endianness - checeks the endianness
- * Return 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
