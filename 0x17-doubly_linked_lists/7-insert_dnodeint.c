b06ba84322d33a43e34a5b2f0a5c9e07779eba08
marsade <adenijifara@gmail.com>
2023-09-14 16:05:25 -0700

    fix betty errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,8 +19,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = *h;
 		count = 1;
 		new->n = n;
-		new->next = NULL;
-		new->prev = NULL;
 		if (current != NULL)
 		{
 			while (current->prev != NULL)
