cd781cd2e117c749cbeb09d45efe5eeaf769a8e3
marsade <adenijifara@gmail.com>
2023-09-14 14:32:30 -0700

    task 5 complete

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * free_dlistint_t - frees a dlistint_t list
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the first node in the list
  */
 void free_dlistint(dlistint_t *head)
 {
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a list
+ * @head: pointer to the head of the list
+ * @index: index of the node
+ * Return: the nth node or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head != NULL) 
+	{
+		if (count == index) 
+			return (head);
+		count++;
+		head = head->next;
+	}
+	return (NULL);
+}
