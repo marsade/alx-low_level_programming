f7e29624822d58dde7ff715cb1219510a8508fc6
marsade <adenijifara@gmail.com>
2023-09-14 14:33:53 -0700

    fix betty errors

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,9 +9,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	while (head != NULL) 
+	while (head != NULL)
 	{
-		if (count == index) 
+		if (count == index)
 			return (head);
 		count++;
 		head = head->next;
