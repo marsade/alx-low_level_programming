bcec2322bb2bb2f75d24ffd3fa48a7bddf28f78b
marsade <adenijifara@gmail.com>
2023-10-07 12:25:24 +0300

    task complete

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = NULL;
 
 	if (ht->array[hash_index] == NULL)
