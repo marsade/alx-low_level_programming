596f0fd0d4911576075cd09dc40c250a36e0ff4b
marsade <adenijifara@gmail.com>
2023-10-07 12:20:39 +0300

    task 3 complete

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with key.
+ * Return: 1 if succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+	hash_node_t *current_node;
+	unsigned long int hash_index;
+
+	hash_index = key_index((const unsigned char *)key, ht->size);
+	if (key[0] == '\0' || key == NULL)
+		return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (ht->array[hash_index] == NULL)
+		ht->array[hash_index] = new_node;
+	else
+	{
+		current_node = ht->array[hash_index];
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+		current_node->next = new_node;
+	}
+	return (1);
+}
