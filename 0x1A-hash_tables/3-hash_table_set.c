ce5dee278143cb37cfb6819dd36ff29745eeb73e
marsade <adenijifara@gmail.com>
2023-10-10 15:17:45 +0300

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,53 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table
- * @key: key
- * @value: value associated with key.
- * Return: 1 if succeeded, 0 otherwise
-*/
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL;
-	char *copy_val;
-	unsigned long int hash_index, i;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (key[0] == '\0' || key == NULL || value == NULL || ht == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	copy_val = strdup(value);
-	if (!copy_val)
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
-	hash_index = key_index((const unsigned char *)key, ht->size);
-	for (i = 0; ht->array[i]; i++)
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(copy_val);
+		free(value_copy);
 		return (0);
 	}
-	new_node->key = strdup(key);
-	if (!new_node->key)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (0);
 	}
-	if (!new_node->value)
-	{
-		free(copy_val);
-		return (0);
-	}
-	new_node->value = copy_val;
-	new_node->next = ht->array[hash_index];
-	ht->array[hash_index] = new_node;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
