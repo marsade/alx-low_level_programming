dad169f6c7998c4872ae68c42fef3dfab0674fdf
marsade <adenijifara@gmail.com>
2024-01-20 16:53:43 +0200

    Task complete

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,5 @@
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 #endif
